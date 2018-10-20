551684adb241829e6359d6fa408e022abb5e16d7
Krzysztof <k.kozlowski.tychy@gmail.com>
2018-10-20 05:57:34 +0200

    Add registered user to file

--- main.cpp
+++ main.cpp
@@ -3,12 +3,11 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     KsiazkaAdresowa ksiazkaAdresowa;
     ksiazkaAdresowa.rejestracjaUzytkownika();
-        ksiazkaAdresowa.rejestracjaUzytkownika();
-            ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
     return 0;
 }
